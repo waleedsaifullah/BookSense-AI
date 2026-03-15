f29d15c4c4badc483fdb749433443011f9cae224
Waleed Saifullah <waleedsaifullah@Waleeds-MacBook-Air.local>
2026-03-15 23:08:44 +0500

    Initial Commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,60 @@
+'use client';
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { SignIn, SignInButton, SignUpButton, UserButton, SignOutButton, Show, useUser } from "@clerk/nextjs"; 
+import { cn } from "@/lib/utils";
+
+const navItems = [
+    { label: "Library", href: "/" },
+    { label: "Add New", href: "/books/new" },
+]
+
+const Navbar = () => {
+    const pathName = usePathname();
+    const { user } = useUser();
+
+    return (
+        <header className="w-full fixed z-50 bg-('--bg-primary')">
+            <div className="wrapper navbar-height py-4 flex justify-between items-center">
+                <Link href="/" className="flex gap-0.5 items-center">
+                    <Image src="/assets/logo.png" alt="Bookified" width={42} height={26} />
+                    <span className="logo-text">Bookified</span>
+                </Link>
+
+                <nav className="w-fit flex gap-7.5 items-center">
+                    {navItems.map(({ label, href }) => {
+                        const isActive = pathName === href || ( href !== '/' && pathName.startsWith(href));
+
+                        return (
+                            <Link href={href} key={label} className={cn('nav-link-base', isActive ? 'nav-link-active' : 'text-black hover:opacity-70')}>
+                                {label}
+                            </Link>
+                        )
+                    })}
+
+                    <div className="flex gap-7.5 items-center">
+                        <Show when="signed-out">
+                            <SignInButton/>
+                        </Show>
+                        <Show when="signed-in">
+                            <div className="nav-user-link">
+                                <UserButton/>
+                                {user?.firstName && (
+                                    <Link href="/subscriptions" className="nav-user-name">
+                                        {user.firstName}
+                                    </Link>
+                                )}
+                            </div>
+                            
+                        </Show>
+                    </div>
+                    
+                    
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
---
+++ types.d.ts
@@ -0,0 +1,6 @@
+export interface BookCardProps {
+    title: string,
+    author: string,
+    coverURL: string,
+    slug: string
+}
